9fd2b46daadd0ad9d984d83a495b8e525491353d
Subhodeep Roy <144267929+SubhodeepRoy17@users.noreply.github.com>
2025-05-26 11:17:49 +0000

    changes done

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
-import Header from "@/components/header"
+
+import React from 'react';
+/*import Header from "@/components/header"
 import HeroSection from "@/components/hero-section"
 import TvPromoSection from "@/components/tv-promo-section"
 import TournamentDetails from "@/components/tournament-details"
@@ -7,20 +9,15 @@ import PrizesSection from "@/components/prizes-section"
 import GameChangerSection from "@/components/game-changer-section"
 import Footer from "@/components/footer"
 import VideoModal from "@/components/video-modal"
-
+*/
 export default function Home() {
   return (
     <div className="min-h-screen bg-[#150237] text-white overflow-hidden" style={{ fontFamily: "Poppins" }}>
-      <Header />
-      <HeroSection />
-      <VideoModal />
-      <TvPromoSection />
-      <TournamentDetails />
-      <PrizesSection />
-      <ImageSection />
-      <GameChangerSection />
-      <Footer />
+      <img 
+        src="/hacked-hack.gif" 
+        alt="Funny Animation" 
+        style={{ maxWidth: '100%', height: 'auto', alignItems: 'center', padding: '500px' }}
+      />
     </div>
   )
 }
-
